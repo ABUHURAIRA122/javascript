abc06eb1948bf6c02ca4d4dce68afa9793a0c503
ABUHURAIRA122 <154108957+ABUHURAIRA122@users.noreply.github.com>
2024-02-28 17:52:43 +0500

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,29 @@
+alert("hello world");
+window.alert("hello world!");
+
+
+// variable for strings 
+// var name = "huraira";
+
+
+// var x = "huraira";
+
+
+// var firstName = "john elia";
+//  var _id = "john";
+ 
+//  console.log(john);
+
+
+
+//practice 2
+
+let gameNum = 22;
+ let user = prompt("guess the enternce number :");
+
+ while (user != gameNum) {
+    user = prompt ("you enterend wrong number. Guess again :");
+
+ }
+
+console.log("congratulation, you enterend the right number");
